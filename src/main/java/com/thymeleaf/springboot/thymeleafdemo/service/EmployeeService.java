e2a319fccad676f7482429c61ad4d527231e7da8
eimantaslilia <eimantaslilia@gmail.com>
2019-05-05 14:45:49 +0100

    spring-boot-thymeleaf-crud-app

---
+++ src/main/java/com/thymeleaf/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.thymeleaf.springboot.thymeleafdemo.service;
+
+
+
+import java.util.List;
+
+import com.thymeleaf.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
